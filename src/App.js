2c4d00bb5299f055a70ce549336c1e26e66b17d7
M-Saad-Sohail <saadsohail232005@gmail.com>
2024-01-31 20:03:48 +0500

    hover effect

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
             ref={passwordRef}
           />
           <button
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            className="outline-none bg-blue-700 hover:bg-blue-900 text-white px-3 py-0.5 shrink-0"
             onClick={copyPasswordToClipboard}
           >Copy</button>
         </div>
